a5d56559007ebf7b03b08a08d844cc22b4b4d3c9
vjain003 <vjain003@ucr.edu>
2018-03-16 03:59:28 +0000

    V4

--- rshell/src/Base.h
+++ rshell/src/Base.h
@@ -1,9 +1,14 @@
 #ifndef __BASE_H__
 #define __BASE_H__
 
+#include <string>
+
+using namespace std;
+
 class Base {
     public:
-        virtual bool execute() = 0;
+        virtual bool execute(int in, int out) = 0;
+        virtual string get_data() = 0;
 };
 
 #endif
